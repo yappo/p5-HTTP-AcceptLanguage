40c57fed3ad224771f45cd8e49618e857f6c30ce
Kazuhiro Osawa <yappo@shibuya.pl>
2013-04-24 22:41:46 +0900

    wrote pod

--- lib/HTTP/AcceptLanguage.pm
+++ lib/HTTP/AcceptLanguage.pm
@@ -118,15 +118,52 @@ __END__
 
 =head1 NAME
 
-HTTP::AcceptLanguage - Blah blah blah
+HTTP::AcceptLanguage - Accept-Language header parser and find available language
 
 =head1 SYNOPSIS
 
   use HTTP::AcceptLanguage;
 
+  my $accept_language = HTTP::AcceptLanguage->new('en, da');
+  $accept_language->match(qw/ da en /); # -> da
+  $accept_language->match(qw/ en da /); # -> en
+
+  # If language quality is the same then order by match method's input list
+  my $accept_language = HTTP::AcceptLanguage->new('en;q=0.5, ja;q=0.1');
+  $accept_language->match(qw/ th da ja /); # -> ja
+  $accept_language->match(qw/ en ja /);    # -> en
+
+  my $accept_language = HTTP::AcceptLanguage->new('en, ja;q=0.3, da;q=1, *;q=0.29, ch-tw');
+  $accept_language->languages; # -> en, da, ch-tw, ja, *
+
+  # create instance for CGI
+  HTTP::AcceptLanguage->new($ENV{HTTP_ACCEPT_LANGUAGE});
+
+  # create instance for raw PSGI
+  HTTP::AcceptLanguage->new($env->{HTTP_ACCEPT_LANGUAGE});
+
+  # create instance for raw Plack::Request
+  HTTP::AcceptLanguage->new($req->header('Accept-Language'));
+
 =head1 DESCRIPTION
 
-HTTP::AcceptLanguage is
+HTTP::AcceptLanguage is HTTP Accept-Language header parser And you can find available language by Accept-Language header.
+
+=head1 METHODS
+
+=head2 new($ENV{HTTP_ACCEPT_LANGUAGE})
+
+It to specify a string of Accept-Language header.
+
+=head2 match(@available_language)
+
+By your available language list, returns the most optimal language.
+
+If language quality is the same, the order of the input list takes precedence.
+
+=head2 languages
+
+Returns are arranged in order of quality language list parsed.
 
 =head1 AUTHOR
 
